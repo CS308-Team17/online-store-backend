c77563c7381d4fe843e5af6806006c915f7e2a5a
Berk Ay <berk.ay2756@gmail.com>
2024-12-30 14:37:28 +0300

    Update RefundRequest.java
    Addressed comment

--- src/main/java/com/example/onlinestore/entity/RefundRequest.java
+++ src/main/java/com/example/onlinestore/entity/RefundRequest.java
@@ -1,7 +1,13 @@
 package com.example.onlinestore.entity;
 
 import com.example.onlinestore.enums.RefundStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class RefundRequest {
     private String refundRequestId;
     private String orderId;
@@ -12,79 +18,4 @@ public class RefundRequest {
     private String reason;
     private String orderDate;
     private RefundStatus status;
-
-    public String getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
-    }
-
-// Getters and setters
-
-
-    public String getRefundRequestId() {
-        return refundRequestId;
-    }
-
-    public void setRefundRequestId(String refundRequestId) {
-        this.refundRequestId = refundRequestId;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public RefundStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(RefundStatus status) {
-        this.status = status;
-    }
 }
