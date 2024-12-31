0dc51e5b327de0a4202e4d394359bcbacaa51177
Efe Koyuncu <efekoyuncu@sabanciuniv.edu>
2024-12-31 16:10:03 +0300

    Implement wishlist discount notifications

--- src/main/java/com/example/onlinestore/constants/CollectionConstants.java
+++ src/main/java/com/example/onlinestore/constants/CollectionConstants.java
@@ -5,4 +5,5 @@ public class CollectionConstants {
     public static final String INVOICES_COLLECTION = "invoices";
     public static final String ORDER_COLLECTION = "orders";
     public static final String COST_COLLECTION = "costs";
+
 }
